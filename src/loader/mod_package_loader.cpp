1c405129907c6e30b90a867ea1c30abaa506b697
David L. <open-source@davidcarbon.dev>
2025-08-04 00:39:15 -0700

    Update Header Files for bit7z v4
    Update CMakeLists.txt

--- src/loader/mod_package_loader.cpp
+++ src/loader/mod_package_loader.cpp
@@ -10,9 +10,9 @@
 #include <fstream>
 #include <memory>
 #include <utility>
-#include <bitextractor.hpp>
+#include <bitfileextractor.hpp>
 #include <fmt/format.h>
-#include <bitarchiveinfo.hpp>
+#include <bitarchivereader.hpp>
 
 static const unsigned char key_xor_table[] = {
         0x94, 0xce, 0xc3, 0xae, 0x73, 0xf9, 0xf1, 0xb9
@@ -74,8 +74,8 @@ std::shared_ptr<mod_package> mod_package_loader::load() {
 
     // Set up 7z interface
     bit7z::Bit7zLibrary lib(L"7z.dll");
-    bit7z::BitExtractor extractor(lib, bit7z::BitFormat::Zip);
-    bit7z::BitArchiveInfo arc(lib, m_path_.wstring(), bit7z::BitFormat::Zip);
+    bit7z::BitFileExtractor extractor(lib, bit7z::BitFormat::Zip);
+    bit7z::BitArchiveReader arc(lib, m_path_.wstring(), bit7z::BitFormat::Zip);
 
     extractor.setPassword(utf8_to_wstring(archive_key));
 
